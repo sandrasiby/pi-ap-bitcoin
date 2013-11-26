df8277e655f2938f11d59172d33d63120e8787f1
sandrasiby <sandrasiby@gmail.com>
2013-11-26 10:56:07 -0500

    Update firewall.sh

--- bitcoinj-test/firewall.sh
+++ bitcoinj-test/firewall.sh
@@ -6,6 +6,8 @@ if [ "$1" = "open" ]; then
 elif [ "$1" = "close" ]; then
    sudo iptables -t mangle -D internet 1 -m mac --mac-source $3 -j RETURN
    sudo rmtrack $2
+elif [ "$1" = "mac" ]; then
+   arp -a $2 | awk '{ print $4 }'
 else
    echo "Error in arguments"
 fi
