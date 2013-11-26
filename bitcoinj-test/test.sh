bfd66076e7dfcf1eb91681c1db5743c92b92241d
sandrasiby <sandrasiby@gmail.com>
2013-11-26 11:25:20 -0500

    Update test.sh

--- bitcoinj-test/test.sh
+++ bitcoinj-test/test.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-if [ "$1" = "testing" ]; then
+if [ "$1" = "mac" ]; then
+   arp -a $2 | awk '{ print $4 }'
+elif [ "$1" = "testing" ]; then
+   echo "Boo"
    sudo iptables -t mangle -N internets
 else
    echo "Error"
